4c4fe52384baef034fcdb5f9035a00ab78d06fde
Håkan Råberg <hakan.raberg@gmail.com>
2023-01-16 16:17:07 +0100

    Evolving parser.

--- src/sql/parser.lisp
+++ src/sql/parser.lisp
@@ -15,36 +15,47 @@
 
 (defrule literal-value (or numeric-literal))
 
-(defrule expr (or (and expr (? ws) (or #\* #\/) (? ws) expr)
-                  (and expr (? ws) (or #\+ #\-) (? ws) expr)
-                  (and #\( (? ws) expr (? ws) #\))
+(defrule expr (or (and expr (? ws) (or "*" "/" "%") (? ws) expr)
+                  (and expr (? ws) (or "+" "-") (? ws) expr)
+                  (and expr (? ws) (or "<>" "<=" ">=" "<"  ">" "=" ) (? ws) expr)
+                  (and "(" (? ws) (or select-stmt expr) (? ws) ")")
+                  (and (~ "CASE") ws
+                       (? (and (not (~ "WHEN")) expr ws))
+                       (+ (and (~ "WHEN") ws expr ws (~ "THEN") ws expr ws))
+                       (? (and (~ "ELSE") ws expr ws))
+                       (~ "END"))
                   literal-value
+                  (and identifier (? ws) "(" (? ws) (or (and (? (and (~  "DISTINCT") ws)) expr (* (and (? ws) "," (? ws) expr)))
+                                                        "*") (? ws) ")")
                   identifier))
 
 (defrule column-def (and identifier (? (and ws identifier))))
 
 (defrule create-table-stmt (and (~ "CREATE") ws (~ "TABLE") ws identifier (? ws)
-                                #\( (? ws) column-def (* (and (? ws) "," (? ws) column-def)) (? ws) #\)))
+                                "(" (? ws) column-def (* (and (? ws) "," (? ws) column-def)) (? ws) ")"))
 
 (defrule insert-stmt (and (~ "INSERT") ws (~ "INTO") ws identifier (? ws)
-                          #\( (? ws) identifier (* (and (? ws) "," (? ws) identifier)) (? ws) #\) (? ws)
-                          (~ "VALUES") (? ws) #\( (? ws) expr (* (and (? ws) "," (? ws) expr)) (? ws) #\)))
+                          "(" (? ws) identifier (* (and (? ws) "," (? ws) identifier)) (? ws) ")" (? ws)
+                          (~ "VALUES") (? ws) "(" (? ws) expr (* (and (? ws) "," (? ws) expr)) (? ws) ")"))
 
 (defrule result-column (or (and expr (? (or (and ws (~ "AS") ws identifier)
                                             (and ws (not (~ "FROM")) identifier))))
-                          #\*))
+                          "*"))
 
 (defrule table-or-subquery (and identifier (? (or (and ws (~ "AS") ws identifier)
                                                   (and ws (not (or (~ "ORDER")
                                                                    (~ "WHERE"))) identifier)))))
 
-(defrule ordering-term (and expr (? (or (~ "ASC") (~ "DESC")))))
+(defrule ordering-term (and expr (? (and ws (or (~ "ASC") (~ "DESC"))))))
 
-(defrule select-stmt (and (~ "SELECT") ws (and result-column (* (and (? ws) "," (? ws) result-column))) ws
-                          (~ "FROM") (? ws) table-or-subquery (* (and (? ws) "," (? ws) table-or-subquery)) ws
-                          (and (~ "ORDER") ws (~ "BY")) (? ws) ordering-term (* (and (? ws) "," (? ws) ordering-term))))
+(defrule select-stmt (and (~ "SELECT") ws (and result-column (* (and (? ws) "," (? ws) result-column)))
+                          (? (and ws (~ "FROM") ws table-or-subquery (* (and (? ws) "," (? ws) table-or-subquery))))
+                          (? (and ws (~ "ORDER") ws (~ "BY") ws ordering-term (* (and (? ws) "," (? ws) ordering-term))))))
 
-(defrule sql-stmt (or create-table-stmt insert-stmt select-stmt))
+(defrule sql-stmt (and (or create-table-stmt
+                           insert-stmt
+                           select-stmt)
+                       (? ws)))
 
 (defun parse-sql (in)
   (esrap:parse 'sql-stmt in))
--- test/sql/parser.lisp
+++ test/sql/parser.lisp
@@ -8,6 +8,9 @@
 (test test-parse-sql
   (is (parse-sql "CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER)"))
   (is (parse-sql "INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104)"))
+  (is (parse-sql "SELECT CASE WHEN c>(SELECT avg(c) FROM t1) THEN a*2 ELSE b*10 END
+  FROM t1
+ ORDER BY 1"))
   (is (parse-sql "SELECT a+b*2+c*3+d*4+e*5,
        (a+b+c+d+e)/5
   FROM t1
