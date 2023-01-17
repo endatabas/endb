b661218b525b497d27d490f3a9610f5c4a4d464e
Håkan Råberg <hakan.raberg@gmail.com>
2023-01-17 22:12:54 +0100

    Making AST more consistent.

--- src/sql/parser.lisp
+++ src/sql/parser.lisp
@@ -24,6 +24,11 @@
           (t items)))
       items))
 
+(defun %flatten-list (name items)
+  (cons name (if (= 1 (length items))
+                 items
+                 (cons (first items) (second items)))))
+
 (defrule ws
     (+ (or #\Space #\Tab #\Newline))
   (:constant nil))
@@ -46,10 +51,15 @@
   (:lambda (items)
     (list :string-literal items)))
 
+(defrule null-literal
+    (~ "NULL")
+  (:lambda (items)
+    (list :null-literal items)))
+
 (defrule literal-value
     (or numeric-literal
         string-literal
-        (~ "NULL")))
+        null-literal))
 
 (defrule expr-or
     (or (and expr-or ws (~ "OR") ws expr-and)
@@ -69,22 +79,22 @@
     (%flatten-ast :expr-not items)))
 
 (defrule expr-compare
-    (and expr-boolean-primary (? ws) (or "<>" "<=" ">=" "<"  ">" "=" ) (? ws) expr-numeric)
+    (and expr-boolean-primary (? ws) (or "<>" "<=" ">=" "<"  ">" "=" ) (? ws) expr-add)
   (:lambda (items)
     (%flatten-ast :expr-compare items)))
 
 (defrule expr-is
-    (and expr-boolean-primary ws (~ "IS") (? (and ws (~ "NOT"))) ws expr-numeric)
+    (and expr-boolean-primary ws (~ "IS") (? (and ws (~ "NOT"))) ws expr-add)
   (:lambda (items)
     (%flatten-ast :expr-is items)))
 
 (defrule expr-between
-    (and expr-boolean-primary ws (? (and (~ "NOT") ws)) (~ "BETWEEN") ws (and expr-numeric ws (~ "AND") ws expr-numeric))
+    (and expr-boolean-primary ws (? (and (~ "NOT") ws)) (~ "BETWEEN") ws (and expr-add ws (~ "AND") ws expr-add))
   (:lambda (items)
     (%flatten-ast :expr-between items)))
 
 (defrule expr-in
-    (and expr-boolean-primary ws (? (and (~ "NOT") ws)) (~ "IN") (? ws) "(" (? ws) expr (* (and (? ws) "," (? ws) expr)) (? ws) ")")
+    (and expr-boolean-primary ws (? (and (~ "NOT") ws)) (~ "IN") (? ws) "(" (? ws) expr-list (? ws) ")")
   (:lambda (items)
     (%flatten-ast :expr-in items)))
 
@@ -130,7 +140,7 @@
     (%flatten-ast :expr-case items)))
 
 (defrule expr-set
-    (and identifier (? ws) "(" (? ws) (or (and (? (and (~ "DISTINCT") ws)) expr (* (and (? ws) "," (? ws) expr)))
+    (and identifier (? ws) "(" (? ws) (or (and (? (and (~ "DISTINCT") ws)) expr-list)
                                           "*") (? ws) ")")
   (:lambda (items)
     (%flatten-ast :expr-set items)))
@@ -138,7 +148,7 @@
 (defrule expr-column
     (and (? (and identifier ".")) identifier)
   (:lambda (items)
-    (%flatten-ast :expr-column items)))
+    (cons :expr-column items)))
 
 (defrule expr-primary
     (or (and "(" (? ws) (or select-stmt expr) (? ws) ")")
@@ -149,62 +159,87 @@
   (:lambda (items)
     (%flatten-ast :expr-primary items)))
 
+(defrule expr-list
+    (and expr (* (and (? ws) "," (? ws) expr)))
+  (:lambda (items)
+    (%flatten-list :expr-list items)))
+
 (defrule expr expr-or)
 
 (defrule column-def
     (and identifier (? (and ws identifier)) (? (and "("  numeric-literal ")")) (? (and ws (~ "PRIMARY") ws (~ "KEY"))))
   (:lambda (items)
-    (%flatten-ast :column-def items)))
+    (cons :column-def items)))
+
+(defrule column-def-list
+    (and column-def (* (and (? ws) "," (? ws) column-def)))
+  (:lambda (items)
+    (%flatten-list :column-def-list items)))
 
 (defrule create-table-stmt
     (and (~ "CREATE") ws (~ "TABLE") ws identifier (? ws)
-         "(" (? ws) column-def (* (and (? ws) "," (? ws) column-def)) (? ws) ")")
+         "(" (? ws) column-def-list (? ws) ")")
   (:lambda (items)
     (%flatten-ast :create-table-stmt items)))
 
 (defrule indexed-column
     (and identifier (? (and ws (or (~ "ASC") (~ "DESC")))))
   (:lambda (items)
-    (%flatten-ast :indexed-column items)))
+    (cons :indexed-column items)))
+
+(defrule indexed-column-list
+    (and indexed-column (* (and (? ws) "," (? ws) indexed-column)))
+  (:lambda (items)
+    (%flatten-list :indexed-column-list items)))
 
 (defrule create-index-stmt
     (and (~ "CREATE") ws (~ "INDEX") ws identifier ws (~ "ON") ws identifier (? ws)
-         "(" (? ws) indexed-column (* (and (? ws) "," (? ws) indexed-column)) (? ws) ")")
+         "(" (? ws) indexed-column-list (? ws) ")")
   (:lambda (items)
     (%flatten-ast :create-index-stmt items)))
 
 (defrule values-stmt
-    (and (~ "VALUES") (? ws) "(" (? ws) expr (* (and (? ws) "," (? ws) expr)) (? ws) ")")
+    (and (~ "VALUES") (? ws) "(" (? ws) expr-list (? ws) ")")
   (:lambda (items)
     (%flatten-ast :values-stmt items)))
 
+(defrule identifier-list
+    (and identifier (* (and (? ws) "," (? ws) identifier)))
+  (:lambda (items)
+    (%flatten-list :identifier-list items)))
+
 (defrule insert-stmt
     (and (~ "INSERT") ws (~ "INTO") ws identifier (? ws)
-         (? (and "(" (? ws) identifier (* (and (? ws) "," (? ws) identifier)) (? ws) ")" (? ws)))
+         (? (and "(" (? ws) identifier-list (? ws) ")" (? ws)))
          values-stmt)
   (:lambda (items)
     (%flatten-ast :insert-stmt items)))
 
+(defrule star
+    "*"
+  (:lambda (items)
+    (list :star items)))
+
 (defrule result-column
     (or (and expr (? (or (and ws (~ "AS") ws identifier)
                          (and ws (! (~ "FROM")) identifier))))
-        "*")
+        star)
   (:lambda (items)
-    (%flatten-ast :result-column items)))
+    (cons :result-column items)))
 
 (defrule table-or-subquery
     (and identifier (? (and ws (or (and (~ "AS") ws identifier)
                                    (and (! (or (~ "ORDER") (~ "WHERE"))) identifier)))))
   (:lambda (items)
-    (%flatten-ast :table-or-subquery items)))
+    (cons :table-or-subquery items)))
 
-(defrule ordering-term
-    (and expr (? (and ws (or (~ "ASC") (~ "DESC")))))
+(defrule table-subquery-list
+    (and table-or-subquery (* (and (? ws) "," (? ws) table-or-subquery)))
   (:lambda (items)
-    (%flatten-ast :ordering-term items)))
+    (%flatten-list :table-subquery-list items)))
 
 (defrule from-clause
-    (and (~ "FROM") ws table-or-subquery (* (and (? ws) "," (? ws) table-or-subquery)))
+    (and (~ "FROM") ws table-subquery-list)
   (:lambda (items)
     (%flatten-ast :from-stmt items)))
 
@@ -213,8 +248,13 @@
   (:lambda (items)
     (%flatten-ast :where-stmt items)))
 
+(defrule result-column-list
+    (and result-column (* (and (? ws) "," (? ws) result-column)))
+  (:lambda (items)
+    (%flatten-list :result-column-list items)))
+
 (defrule select-core
-    (or (and (~ "SELECT") ws (and result-column (* (and (? ws) "," (? ws) result-column)))
+    (or (and (~ "SELECT") ws result-column-list
              (? (and ws from-clause))
              (? (and ws where-clause)))
         values-stmt)
@@ -226,8 +266,18 @@
   (:lambda (items)
     (%flatten-ast :compound-select-stmt items)))
 
+(defrule ordering-term
+    (and expr (? (and ws (or (~ "ASC") (~ "DESC")))))
+  (:lambda (items)
+    (cons :ordering-term items)))
+
+(defrule ordering-term-list
+    (and ordering-term (* (and (? ws) "," (? ws) ordering-term)))
+  (:lambda (items)
+    (%flatten-list :ordering-term-list items)))
+
 (defrule order-by-clause
-    (and (~ "ORDER") ws (~ "BY") ws ordering-term (* (and (? ws) "," (? ws) ordering-term)))
+    (and (~ "ORDER") ws (~ "BY") ws ordering-term-list)
   (:lambda (items)
     (%flatten-ast :order-by-clause items)))
 
