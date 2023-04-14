206ede38bb209e86388f442c2f862e924272bf24
Håkan Råberg <hakan.raberg@gmail.com>
2023-04-14 11:24:24 +0200

    Move to CLOS. Add extensible sequences protocol.

--- src/arrow.lisp
+++ src/arrow.lisp
@@ -3,9 +3,6 @@
   (:import-from :trivial-utf-8))
 (in-package :endb/arrow)
 
-(defstruct arrow-array
-  (data-type nil :type string :read-only t))
-
 (defgeneric arrow-push (array x))
 (defgeneric arrow-push-null (array))
 (defgeneric arrow-valid-p (array n))
@@ -13,10 +10,47 @@
 (defgeneric arrow-value (array n))
 (defgeneric arrow-length (array))
 (defgeneric arrow-null-count (array))
+(defgeneric arrow-data-type (array))
 
-(defstruct (primitive-array (:include arrow-array))
-  (validity (make-array 0 :element-type 'bit :fill-pointer 0) :type bit-vector :read-only t)
-  (values nil :type vector :read-only t))
+(defclass arrow-array (sequence standard-object) ())
+
+(defmethod print-object ((obj arrow-array) stream)
+  (print-unreadable-object (obj stream :type t)
+    (format stream "~a" (coerce obj 'vector))))
+
+(defmethod sequence:elt ((array arrow-array) (n fixnum))
+  (arrow-get array n))
+
+(defmethod sequence:length ((array arrow-array))
+  (arrow-length array))
+
+(defmethod (setf sequence:elt) (x (array arrow-array) (n fixnum))
+  (if (= n (arrow-length array))
+      (arrow-push array x)
+      (call-next-method)))
+
+(defmethod sequence:make-sequence-like ((array arrow-array) length &key (initial-element nil initial-element-p) (initial-contents nil initial-contents-p))
+  (let ((new-array (make-instance (type-of array))))
+    (cond
+      (initial-element-p
+       (dotimes (n length)
+         (arrow-push new-array initial-element)))
+      (initial-contents-p
+       (dolist (x (subseq initial-contents 0 (min length (length initial-contents))))
+         (arrow-push new-array x)))
+      (t
+       (dotimes (n (min length (length array)))
+         (if (arrow-valid-p array n)
+             (arrow-push new-array (arrow-value array n))
+             (arrow-push-null new-array)))))
+    (loop
+      (if (< (arrow-length new-array) length)
+          (arrow-push-null new-array)
+          (return new-array)))))
+
+(defclass primitive-array (arrow-array)
+  ((validity :initform (make-array 0 :element-type 'bit :fill-pointer 0) :type bit-vector)
+   (values :type vector)))
 
 (defmethod arrow-valid-p ((array primitive-array) (n fixnum))
   (with-slots (validity) array
@@ -45,9 +79,8 @@
   (with-slots (validity) array
     (count-if #'zerop validity)))
 
-(defstruct (int64-array (:include primitive-array
-                         (data-type "l")
-                         (values (make-array 0 :element-type '(signed-byte 64) :fill-pointer 0) :type (vector (signed-byte 64))))))
+(defclass int64-array (primitive-array)
+  ((values :initform (make-array 0 :element-type '(signed-byte 64) :fill-pointer 0) :type (vector (signed-byte 64)))))
 
 (defmethod arrow-push ((array int64-array) (x integer))
   (with-slots (values) array
@@ -58,9 +91,11 @@
 (defmethod arrow-value ((array int64-array) (n fixnum))
   (aref (slot-value array 'values) n))
 
-(defstruct (float64-array (:include primitive-array
-                           (data-type "g")
-                           (values (make-array 0 :element-type 'double-float :fill-pointer 0) :type (vector double-float)))))
+(defmethod arrow-data-type ((array int64-array))
+  "l")
+
+(defclass float64-array (primitive-array)
+  ((values :initform (make-array 0 :element-type 'double-float :fill-pointer 0) :type (vector double-float))))
 
 (defmethod arrow-push ((array float64-array) (x float))
   (with-slots (values) array
@@ -71,9 +106,11 @@
 (defmethod arrow-value ((array float64-array) (n fixnum))
   (aref (slot-value array 'values) n))
 
-(defstruct (boolean-array (:include primitive-array
-                           (data-type "b")
-                           (values (make-array 0 :element-type 'bit :fill-pointer 0) :type (vector bit)))))
+(defmethod arrow-data-type ((array float64-array))
+  "g")
+
+(defclass boolean-array (primitive-array)
+  ((values :initform (make-array 0 :element-type 'bit :fill-pointer 0) :type (vector bit))))
 
 (defmethod arrow-push ((array boolean-array) x)
   (with-slots (values) array
@@ -84,11 +121,13 @@
 (defmethod arrow-value ((array boolean-array) (n fixnum))
   (= 1 (aref (slot-value array 'values) n)))
 
-(defstruct (binary-array (:include arrow-array
-                          (data-type "z" :type string :read-only t)))
-  (validity (make-array 0 :element-type 'bit :fill-pointer 0) :type bit-vector :read-only t)
-  (offsets (make-array 1 :element-type '(signed-byte 32) :fill-pointer 1) :type (vector (signed-byte 32)) :read-only t)
-  (data (make-array 0 :element-type '(unsigned-byte 8) :fill-pointer 0) :type (vector (unsigned-byte 8)) :read-only t))
+(defmethod arrow-data-type ((array boolean-array))
+  "b")
+
+(defclass binary-array (arrow-array)
+  ((validity :initform (make-array 0 :element-type 'bit :fill-pointer 0) :type bit-vector)
+   (offsets :initform (make-array 1 :element-type '(signed-byte 32) :fill-pointer 1) :type (vector (signed-byte 32)))
+   (data :initform (make-array 0 :element-type '(unsigned-byte 8) :fill-pointer 0) :type (vector (unsigned-byte 8)))))
 
 (defmethod arrow-valid-p ((array binary-array) (n fixnum))
   (with-slots (validity) array
@@ -128,8 +167,10 @@
   (with-slots (validity) array
     (count-if #'zerop validity)))
 
-(defstruct (utf8-array (:include binary-array
-                        (data-type "u"))))
+(defmethod arrow-data-type ((array binary-array))
+  "z")
+
+(defclass utf8-array (binary-array) ())
 
 (defmethod arrow-push ((array utf8-array) (x string))
   (with-slots (validity offsets data) array
@@ -147,16 +188,28 @@
            (end (aref offsets (1+ n))))
       (trivial-utf-8:utf-8-bytes-to-string data :start start :end end))))
 
-;; (arrow-get (arrow-push (arrow-push-null (make-int64-array)) 1) 0)
+(defmethod arrow-data-type ((array utf8-array))
+  "u")
 
-;; (arrow-get (arrow-push (arrow-push-null (make-float64-array)) 1.0d0) 1)
+;; (pprint (arrow-push (arrow-push-null (make-instance 'int64-array)) 1))
 
-;; (arrow-get (arrow-push (arrow-push-null (make-boolean-array)) t) 1)
+;; (loop for x being the element in (arrow-push (arrow-push-null (make-instance 'int64-array)) 1)
+;;       collect x)
 
-;; (arrow-get (arrow-push (arrow-push (arrow-push-null (make-binary-array)) #(0 1 2))  #(3 4)) 2)
+;; (setf (elt (arrow-push (arrow-push-null (make-instance 'int64-array)) 1) 2) 2)
 
-;; (arrow-get (arrow-push (arrow-push (arrow-push-null (make-utf8-array)) "hello") "world") 2)
+;; (arrow-get (arrow-push (arrow-push-null (make-instance 'int64-array)) 1) 0)
 
-;; (arrow-null-count (arrow-push (arrow-push (arrow-push-null (make-utf8-array)) "hello") "world"))
+;; (arrow-get (arrow-push (arrow-push-null (make-instance 'float64-array)) 1.0d0) 1)
 
-;; (arrow-length (arrow-push (arrow-push (arrow-push-null (make-utf8-array)) "hello") "world"))
+;; (arrow-get (arrow-push (arrow-push-null (make-instance 'boolean-array)) t) 1)
+
+;; (arrow-get (arrow-push (arrow-push (arrow-push-null (make-instance 'binary-array)) #(0 1 2))  #(3 4)) 2)
+
+;; (arrow-get (arrow-push (arrow-push (arrow-push-null (make-instance 'utf8-array)) "hello") "world") 2)
+
+;; (arrow-null-count (arrow-push (arrow-push (arrow-push-null (make-instance 'utf8-array)) "hello") "world"))
+
+;; (arrow-length (arrow-push (arrow-push (arrow-push-null (make-instance 'utf8-array)) "hello") "world"))
+
+;; (arrow-data-type (make-instance 'utf8-array))
