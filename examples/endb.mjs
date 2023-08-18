6b48fdc045166a26355fa1ea9d1b4c0ddc957014
Håkan Råberg <hakan.raberg@gmail.com>
2023-08-18 08:28:35 +0200

    Fix and tweak to JS client example.

--- examples/endb.mjs
+++ examples/endb.mjs
@@ -35,7 +35,7 @@ function jsonLDDecoder(x) {
     return x;
 }
 
-async function sql(q, {p = [], accept = 'application/ld+json', auth = [], url = 'http://localhost:3803/sql'} = {}) {
+async function sql(q, p = [], {accept = 'application/ld+json', auth = [], url = 'http://localhost:3803/sql'} = {}) {
     const body = new FormData();
 
     body.append('q', q);
@@ -50,7 +50,7 @@ async function sql(q, {p = [], accept = 'application/ld+json', auth = [], url = 
     const response = await fetch(url, {method: 'POST', headers: headers, body: body});
 
     if (response.ok) {
-        if accept === 'text/csv' {
+        if (accept === 'text/csv') {
             return await response.text();
         } else {
             return JSON.parse(await response.text(), (k, v) => jsonLDDecoder(v));
