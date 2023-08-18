36b3535eba01dd7c22ac8dda6b6aa5c96212182d
Håkan Råberg <hakan.raberg@gmail.com>
2023-08-18 15:36:43 +0200

    Refactor python to hide implementation details.

--- examples/endb.py
+++ examples/endb.py
@@ -6,35 +6,6 @@ import json
 import urllib.parse
 import urllib.request
 
-def from_json_ld(obj):
-    match obj.get('@type', None):
-        case 'xsd:dateTime':
-            return datetime.fromisoformat(obj['@value'].replace('Z', '+00:00'))
-        case 'xsd:date':
-            return date.fromisoformat(obj['@value'])
-        case 'xsd:time':
-            return time.fromisoformat(obj['@value'])
-        case 'xsd:base64Binary':
-            return base64.b64decode(obj['@value'])
-        case 'xsd:integer':
-            return int(obj['@value'])
-        case _:
-            return obj.get('@graph', obj)
-
-class JSONLDEncoder(json.JSONEncoder):
-    def default(self, obj):
-        match obj:
-           case datetime():
-               return {'@value': datetime.isoformat(obj), '@type': 'xsd:dateTime'}
-           case date():
-               return {'@value': date.isoformat(obj), '@type': 'xsd:date'}
-           case time():
-               return {'@value': time.isoformat(obj), '@type': 'xsd:time'}
-           case bytes():
-               return {'@value': base64.b64encode(obj).decode(), '@type': 'xsd:base64Binary'}
-           case _:
-               return super().default(obj)
-
 class Endb:
     def __init__(self, url='http://localhost:3803/sql', accept='application/ld+json', username=None, password=None):
         super().__init__()
@@ -42,6 +13,35 @@ class Endb:
         self.accept = accept
         self.username = username
         self.password = password
+
+    def __from_json_ld(self, obj):
+        match obj.get('@type', None):
+            case 'xsd:dateTime':
+                return datetime.fromisoformat(obj['@value'].replace('Z', '+00:00'))
+            case 'xsd:date':
+                 return date.fromisoformat(obj['@value'])
+            case 'xsd:time':
+                 return time.fromisoformat(obj['@value'])
+            case 'xsd:base64Binary':
+                 return base64.b64decode(obj['@value'])
+            case 'xsd:integer':
+                 return int(obj['@value'])
+            case _:
+                 return obj.get('@graph', obj)
+
+    class __JSONLDEncoder(json.JSONEncoder):
+        def default(self, obj):
+            match obj:
+               case datetime():
+                   return {'@value': datetime.isoformat(obj), '@type': 'xsd:dateTime'}
+               case date():
+                   return {'@value': date.isoformat(obj), '@type': 'xsd:date'}
+               case time():
+                    return {'@value': time.isoformat(obj), '@type': 'xsd:time'}
+               case bytes():
+                    return {'@value': base64.b64encode(obj).decode(), '@type': 'xsd:base64Binary'}
+               case _:
+                    return super().default(obj)
 
     def sql(self, q, p=[], accept=None):
         if accept is None:
@@ -51,7 +51,7 @@ class Endb:
             auth_base64 = base64.b64encode(bytes('%s:%s' % (self.username, self.password), 'ascii'))
             headers['Authorization'] = 'Basic %s' % auth_base64.decode('utf-8')
 
-        payload = {'q': q, 'p': json.dumps(p, cls=JSONLDEncoder)}
+        payload = {'q': q, 'p': json.dumps(p, cls=Endb.__JSONLDEncoder)}
         data = urllib.parse.urlencode(payload)
         data = data.encode('ascii')
 
@@ -60,9 +60,9 @@ class Endb:
             if accept == 'text/csv':
                 return response.read().decode()
             else:
-                return json.loads(response.read(), object_hook=from_json_ld)
+                return json.loads(response.read(), object_hook=self.__from_json_ld)
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
-        print(Endb().sql(sys.argv[1], [9223372036854775808]))
+        print(Endb().sql(sys.argv[1]))
