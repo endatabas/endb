f90a9a7aa3298976aa1d088b5501fd5cadfece46
Håkan Råberg <hakan.raberg@gmail.com>
2023-10-09 15:09:57 +0200

    Porting over parts of the insta tests to the CST parser and tweak it to make them pass.

--- lib/endb_cst/src/sql.rs
+++ lib/endb_cst/src/sql.rs
@@ -3,12 +3,12 @@ use endb_proc_macro::peg;
 
 peg! {
 
-    <whitespace> <- ~"(\\s*|--[^\n\r]*?)*";
+    <whitespace> <- ~"(\\s+|--[^\n\r]*)*";
 
     <ident> <- #"\\b\\p{XID_START}\\p{XID_CONTINUE}*\\b";
 
     numeric_literal <- #"\\b(0[xX][0-9A-Fa-f]+|[0-9]+(\\.[0-9]+)?([eE][+-]?[0-9]+)?)\\b";
-    string_literal <- #"(\"(?:\\\"|[^\"])*?\"|'(?:''|[^'])*?')";
+    string_literal <- #"(\"(?:\\\"|[^\"])*\"|'(?:''|[^'])*')";
     blob_literal <- #"(\\b[xX]'[0-9A-Fa-f]*?'|[xX]\"[0-9A-Fa-f]*?\")";
 
     iso_time_literal <- #"\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?";
@@ -17,21 +17,21 @@ peg! {
 
     time_literal <- TIME ^ (#"('\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?'|\"\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?\")");
     date_literal <- DATE ^ (#"('\\d{4}-\\d{2}-\\d{2}'|\"\\d{4}-\\d{2}-\\d{2}\")");
-    timestamp_literal <- TIMESTAMP ^ (#"('\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?Z?'|\"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?Z?\")");
+    timestamp_literal <- TIMESTAMP ^ (#"('\\d{4}-\\d{2}-\\d{2}[T ]\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?Z?'|\"\\d{4}-\\d{2}-\\d{2}[T ]\\d{2}:\\d{2}:\\d{2}(:?\\.\\d+)?Z?\")");
 
     iso_duration_literal <- #"P(\\d+(:?[,.]\\d+)?Y)?(:?\\d+(:?[,.]\\d+)?M)?(:?\\d+(:?[,.]\\d+)?D)?(T(\\d+(:?[,.]\\d+)?H)?(:?\\d+(:?[,.]\\d+)?M)?(:?\\d+(:?[,.]\\d+)?S)?)?";
 
     datetime_field <- YEAR / MONTH / DAY / HOUR / MINUTE / SECOND;
-    interval_literal <- INTERVAL ^ (#"('\\d+(:?-\\d+)?'|\"\\d+(:?-\\d+)?\")|('(:?\\d+ )?\\d{2}(:?\\:\\d{2})?(:?\\:\\d{2})?(:?\\.\\d+)?'|\"(:?\\d+ )?\\d{2}(:?\\:\\d{2})?(:?\\:\\d{2})?(:?\\.\\d+)?\")") datetime_field TO datetime_field;
+    interval_literal <- INTERVAL ^ (#"('\\d+(:?-\\d+)?'|\"\\d+(:?-\\d+)?\")|('(:?\\d+ )?\\d{2}(:?\\:\\d{2})?(:?\\:\\d{2})?(:?\\.\\d+)?'|\"(:?\\d+ )?\\d{2}(:?\\:\\d{2})?(:?\\:\\d{2})?(:?\\.\\d+)?\")") datetime_field ( TO ^datetime_field )?;
 
     <literal> <-
-        iso_time_literal
+        iso_timestamp_literal
         / iso_date_literal
-        / iso_timestamp_literal
-        / time_literal
-        / date_literal
-        / timestamp_literal
+        / iso_time_literal
         / iso_duration_literal
+        / timestamp_literal
+        / date_literal
+        / time_literal
         / interval_literal
         / numeric_literal
         / string_literal
@@ -43,7 +43,7 @@ peg! {
         / CURRENT_DATE
         / CURRENT_TIMESTAMP;
 
-    bind_parameter <- #"\\b(:?\\?|:\\p{XID_START}\\p{XID_CONTINUE}*)\\b";
+    bind_parameter <- #"(:?\\?|:\\p{XID_START}\\p{XID_CONTINUE}*\\b)";
 
     function_name <- ident;
     type_name <- ident;
@@ -53,7 +53,7 @@ peg! {
     paren_expr <- "(" expr ")";
     extract_expr <- EXTRACT ^( "(" datetime_field FROM expr ")" );
     cast_expr <- CAST ^( "(" expr AS type_name ")" );
-    function_call_expr <- function_name "(" ( DISTINCT? ( expr ( "," expr )* )? / "*" ) order_by_clause? ")" ( FILTER ^( "(" WHERE expr ")" ) )?;
+    function_call_expr <- function_name "(" ( DISTINCT? ( ( expr / "*" ) ( "," expr )* )? / "*" ) order_by_clause? ")" ( FILTER ^( "(" WHERE expr ")" ) )?;
     exists_expr <- EXISTS ^subquery;
     case_when_then_expr <- WHEN expr THEN expr;
     case_expr <- CASE ^( (!WHEN expr)? case_when_then_expr+ ( ELSE expr )? END );
@@ -61,8 +61,8 @@ peg! {
 
     array_expr <- ARRAY subquery / ARRAY? ( "[" "]" / "[" "..."? expr  ( "," "..."? expr )* ","? "]" );
     object_key_value_pair <- ( ( ident / string_literal / "["  expr "]" ) (#"[:=]") expr ) / "..." expr / ( table_name "." "*" ) / column_reference / bind_parameter;
-    object_expr <- OBJECT "(" object_key_value_pair ( "," object_key_value_pair )* ","? ")" / "{" object_key_value_pair ( "," object_key_value_pair )* ","? "}";
-    path_expr <- "$" ( ( "." ident ) / "[" ( "#" "-" )? expr "]" )* ;
+    object_expr <- OBJECT "(" ( object_key_value_pair ( "," object_key_value_pair )* ","? )? ")" / "{" ( object_key_value_pair ( "," object_key_value_pair )* ","? )? "}";
+    path_expr <- "$" ( ( "." ident ) / "[" ( "#" "-" )? expr "]" / "[" "#" "]" )* ;
 
     <atom> <-
         literal
@@ -79,7 +79,7 @@ peg! {
         / case_expr
         / column_reference;
 
-    <access> <- atom ( ( ".." / "." ) ( ident / "[" ( expr / "*" ) "]" ) )*;
+    <access> <- atom ( ( ".." / "." ) ident / "[" ( expr / "*" ) "]" )*;
     <unary> <- ("+" / "-" / "~" )* access;
     <concat> <- unary ( "||" unary )*;
     <mul> <- concat ( ( "*" / "/" / "%" ) concat )*;
@@ -91,6 +91,7 @@ peg! {
             ( ( "==" / "=" / "!=" / "<>" / OVERLAPS / EQUALS / CONTAINS / IMMEDIATELY? PRECEDES / IMMEDIATELY? SUCCEEDS ) comp )
                 / NOT? ( LIKE ^( comp ( ESCAPE comp )? ) / ( GLOB / REGEXP / MATCH / "@>" ) ^comp )
                 / IS ^( NOT? comp )
+                / NOT NULL
                 / NOT? BETWEEN ^( comp AND comp )
                 / NOT? IN ^( "(" select_stmt ")" / "(" expr ( "," expr )* ")" / "(" ")" )
         )*;
@@ -102,7 +103,7 @@ peg! {
     expr <- or;
 
     column_alias <- ident;
-    table_name <- ident / INFORMATION_SCEHMA "." ident;
+    table_name <- INFORMATION_SCHEMA "." ident / ident;
 
     qualified_asterisk <- table_name "." ^"*";
     asterisk <- "*";
@@ -114,14 +115,14 @@ peg! {
     join_constraint <- ( ON expr )?;
     join_operator <- "," / ( LEFT OUTER? / INNER / CROSS )? JOIN;
     join_clause <- table_or_subquery ( join_operator table_or_subquery join_constraint )*;
-    invalid_table_alias <- LEFT / INNER / CROSS/ JOIN / WHERE / GROUP / HAVING / ORDER / LIMIT / ON / UNION / INTERSECT / EXCEPT;
+    invalid_table_alias <- LEFT / INNER / CROSS / JOIN / WHERE / GROUP / HAVING / ORDER / LIMIT / ON / UNION / INTERSECT / EXCEPT;
     table_or_subquery <-
-        table_name ( NOT ^INDEXED )?  ( FOR ^( SYSTEM TIME ( ALL / AS OF atom / FROM atom TO atom / BETWEEN atom AND atom )) )?( AS ^table_alias / !invalid_table_alias table_alias )?
+        UNNEST "(" expr ( "," expr )* ")" ( WITH ^ORDINALITY )? AS table_alias
+        / table_name ( NOT ^INDEXED )?  ( FOR ^( SYSTEM_TIME ( ALL / AS OF atom / FROM atom TO atom / BETWEEN atom AND atom ) ) )? ( AS ^table_alias / !invalid_table_alias table_alias )?
         / "(" select_stmt ")" AS table_alias
-        / UNNEST "(" expr ("," expr)* ")" ( WITH ^ORDINALITY )? AS table_alias
         / "(" join_clause ")";
 
-    from_clause <- FROM ( table_or_subquery ( "," table_or_subquery )* / join_clause );
+    from_clause <- FROM join_clause;
     where_clause <- WHERE expr;
     group_by_clause <- GROUP BY expr ( "," expr )*;
     having_clause <- HAVING expr;
@@ -147,9 +148,9 @@ peg! {
         order_by_clause?
         limit_offset_clause?;
 
-    update_body <- ( SET ( column_name / path_expr ) "=" expr ( "," ( column_name / path_expr ) "=" expr ) )? ( ( UNSET / REMOVE) ( column_name / path_expr ) ( "," ( column_name / path_expr ) ) )? ( PATCH? object_expr )? ( WHERE expr )?;
+    update_body <- ( SET ( column_name / path_expr ) "=" expr ( "," ( column_name / path_expr ) "=" expr )* )? ( ( UNSET / REMOVE) ( column_name / path_expr ) ( "," ( column_name / path_expr ) )* )? ( PATCH? object_expr )? ( WHERE expr )?;
 
-    insert_stmt <- INSERT ( OR ^REPLACE )? INTO table_name ( ( "(" column_name ( "," column_name) ")" )? select_stmt / OBJECTS? object_expr ( "," object_expr)* )
+    insert_stmt <- INSERT ( OR ^REPLACE )? INTO table_name ( ( "(" column_name ( "," column_name)* ")" )? select_stmt / OBJECTS? object_expr ( "," object_expr)* )
         ( ON ^CONFLICT "(" column_name ("," column_name )* ")" DO ( NOTHING / UPDATE update_body ) )?;
 
     delete_stmt <- DELETE FROM table_name ( WHERE expr )?;
@@ -157,7 +158,7 @@ peg! {
     update_stmt <- UPDATE table_name update_body;
 
     create_index_stmt <- CREATE UNIQUE? INDEX ident ON table_name "(" ordering_term ( "," ordering_term )* ")";
-    create_view_stmt <- CREATE ( TEMPORARY / TEMP )? VIEW ident "(" column_name ( "," column_name ) ")" AS select_stmt;
+    create_view_stmt <- CREATE ( TEMPORARY / TEMP )? VIEW ident ( "(" column_name ( "," column_name )* ")" )? AS select_stmt;
 
     column_definition <- PRIMARY KEY "(" column_name ( "," column_name )* ")" / FOREIGN KEY "(" column_name ( "," column_name )* ")" REFERENCES table_name "(" column_name ( "," column_name )* ")" / column_name type_name ( "(" numeric_literal ")" )? ( PRIMARY KEY )? UNIQUE?;
     create_table_stmt <- CREATE TABLE table_name "(" column_definition ( "," column_definition )* ")";
