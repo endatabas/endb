15a5d4476c79df92f53e931cc828e9f2f29a5acc
Håkan Råberg <hakan.raberg@gmail.com>
2023-10-11 09:13:45 +0200

    Refactor CST grammar.

--- test/lib/cst.lisp
+++ test/lib/cst.lisp
@@ -9,12 +9,13 @@
 (in-suite* :lib)
 
 (test parse-cst
-  (is (equal '(:|sql_stmt_list|
-               (:|sql_stmt|
-                (:|select_stmt|
-                 (:|select_core| ("SELECT" 0 6)
-                  (:|result_column| (:|expr| (:|numeric_literal| ("1" 7 8))))))))
-             (parse-sql-cst "SELECT 1"))))
+      (is (equal '(:|sql_stmt_list|
+                    (:|sql_stmt|
+                      (:|select_stmt|
+                        (:|select_core| ("SELECT" 0 6)
+                          (:|result_expr_list|
+                            (:|result_column| (:|expr| (:|numeric_literal| ("1" 7 8)))))))))
+                 (parse-sql-cst "SELECT 1"))))
 
 (defvar report-json
   "{\"kind\":\"Error\",\"msg\":\"parse error: unexpected SEL\",\"note\":\"/ sql_stmt_list / sql_stmt / select_stmt / with_clause\",\"location\":[\"/sql\",0],\"labels\":[{\"span\":[\"/sql\",{\"start\":0,\"end\":3}],\"msg\":\"expected WITH\",\"color\":\"Red\",\"order\":0,\"priority\":0},{\"span\":[\"/sql\",{\"start\":0,\"end\":0}],\"msg\":\"while parsing with_clause\",\"color\":\"Blue\",\"order\":1,\"priority\":0}],\"source\":\"SEL\"}")
