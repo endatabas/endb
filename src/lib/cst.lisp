0f4f7c8ce49f0a7784cb48754dd7f28d50725d0e
Håkan Råberg <hakan.raberg@gmail.com>
2023-10-11 11:23:30 +0200

    Start of CST to AST translation.

--- src/lib/cst.lisp
+++ src/lib/cst.lisp
@@ -1,11 +1,12 @@
 (defpackage :endb/lib/cst
   (:use :cl)
-  (:export  #:parse-sql-cst #:render-error-report)
+  (:export  #:parse-sql-cst #:render-error-report #:cst->ast)
   (:import-from :endb/lib)
   (:import-from :endb/lib/parser)
   (:import-from :endb/json)
   (:import-from :cffi)
-  (:import-from :trivial-utf-8))
+  (:import-from :trivial-utf-8)
+  (:import-from :trivia))
 (in-package :endb/lib/cst)
 
 (cffi:defcfun "endb_parse_sql_cst" :void
@@ -109,3 +110,89 @@
                                      (cffi:callback render-json-error-report-on-success)
                                      (cffi:callback render-json-error-report-on-error)))
     (endb/lib/parser:strip-ansi-escape-codes result)))
+
+(defun cst->ast (input cst)
+  (labels ((strip-delimiters (delimiter xs)
+             (remove-if (lambda (x)
+                          (and (listp x) (= 3 (length x)) (equal delimiter (first x))))
+                        xs))
+           (ident (id start end)
+             (let ((s (make-symbol id)))
+               (setf (get s :start) start (get s :end) end (get s :input) input)
+               s))
+           (walk (cst)
+             (trivia:ematch cst
+               ((list :|sql_stmt_list| x)
+                (first (walk x)))
+
+               ((list* :|sql_stmt_list| xs)
+                (mapcar #'walk (strip-delimiters ";" xs)))
+
+               ((list :|sql_stmt| x)
+                (walk x))
+
+               ((list* :|select_stmt| xs)
+                (list (mapcan #'walk xs)))
+
+               ((list* :|select_core| _ xs)
+                (cons :select (mapcan #'walk xs)))
+
+               ((list* :|result_expr_list| xs)
+                (list (mapcar #'walk (strip-delimiters "," xs))))
+
+               ((list :|result_column| x)
+                (list (walk x)))
+
+               ((list* :|from_clause| _ xs)
+                (cons :from (mapcar #'walk xs)))
+
+               ((list* :|join_clause| xs)
+                (mapcar #'walk xs))
+
+               ((list :|table_or_subquery| x)
+                (list (walk x)))
+
+               ((list :|table_name| (list x start end))
+                (ident x start end))
+
+               ((list :|where_clause| _ expr)
+                (list :where (walk expr)))
+
+               ((list* :|order_by_clause| _ _ xs)
+                (list :order-by (mapcar #'walk (strip-delimiters "," xs))))
+
+               ((list :|ordering_term| x (list dir _ _))
+                (list (walk x) (intern dir :keyword)))
+
+               ((list :|ordering_term| x)
+                (list (walk x) :asc))
+
+               ((list :|expr| x)
+                (walk x))
+
+               ((list :|expr| x (list op _ _) y)
+                (list (intern op :keyword) (walk x) (walk y)))
+
+               ((list* :|function_call_expr| xs)
+                (cons :function (mapcar #'walk (strip-delimiters ")" (strip-delimiters "(" xs)))))
+
+               ((list* :|expr_list| xs)
+                (mapcar #'walk xs))
+
+               ((list :|column_reference| x)
+                (walk x))
+
+               ((list :|column_name| (list x start end))
+                (ident x start end))
+
+               ((list :|function_name| (list x start end))
+                (ident x start end))
+
+               ((list :|numeric_literal| (list x _ _))
+                (read-from-string x))
+
+               ((list* :ast xs)
+                xs))))
+    (let ((*read-eval* nil)
+          (*read-default-float-format* 'double-float))
+      (walk cst))))
