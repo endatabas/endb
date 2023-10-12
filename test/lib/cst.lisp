e8a620fcea6eb9ac2764fd5ea510bd9b965699cd
Håkan Råberg <hakan.raberg@gmail.com>
2023-10-12 08:48:45 +0200

    More CST to AST translation.

--- test/lib/cst.lisp
+++ test/lib/cst.lisp
@@ -68,26 +68,26 @@
   (is-valid "SELECT 1")
   (is-valid "SELECT a, b, 123, myfunc(b) FROM table_1 WHERE a > b AND b < 100 ORDER BY a DESC, b")
 
-;;   (is-valid "CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER)")
-;;   (is-valid "INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104)")
-;;   (is-valid "SELECT CASE WHEN c>(SELECT avg(c) FROM t1) THEN a*2 ELSE b*10 END
-;;   FROM t1
-;;  ORDER BY 1")
-;;   (is-valid "SELECT a+b*2+c*3+d*4+e*5,
-;;        (a+b+c+d+e)/5
-;;   FROM t1
-;;  ORDER BY 1,2")
-;;   (is-valid "SELECT a+b*2+c*3+d*4+e*5,
-;;        CASE WHEN a<b-3 THEN 111 WHEN a<=b THEN 222
-;;         WHEN a<b+3 THEN 333 ELSE 444 END,
-;;        abs(b-c),
-;;        (a+b+c+d+e)/5,
-;;        a+b*2+c*3
-;;   FROM t1
-;;  WHERE (e>c OR e<d)
-;;    AND d>e
-;;    AND EXISTS(SELECT 1 FROM t1 AS x WHERE x.b<t1.b)
-;;  ORDER BY 4,2,1,3,5")
+  (is-valid "CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER)")
+  (is-valid "INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104)")
+  (is-valid "SELECT CASE WHEN c>(SELECT avg(c) FROM t1) THEN a*2 ELSE b*10 END
+  FROM t1
+ ORDER BY 1")
+  (is-valid "SELECT a+b*2+c*3+d*4+e*5,
+       (a+b+c+d+e)/5
+  FROM t1
+ ORDER BY 1,2")
+  (is-valid "SELECT a+b*2+c*3+d*4+e*5,
+       CASE WHEN a<b-3 THEN 111 WHEN a<=b THEN 222
+        WHEN a<b+3 THEN 333 ELSE 444 END,
+       abs(b-c),
+       (a+b+c+d+e)/5,
+       a+b*2+c*3
+  FROM t1
+ WHERE (e>c OR e<d)
+   AND d>e
+   AND EXISTS(SELECT 1 FROM t1 AS x WHERE x.b<t1.b)
+ ORDER BY 4,2,1,3,5")
 ;;   (is-valid "SELECT CASE a+1 WHEN b THEN 111 WHEN c THEN 222
 ;;         WHEN d THEN 333  WHEN e THEN 444 ELSE 555 END,
 ;;        CASE WHEN a<b-3 THEN 111 WHEN a<=b THEN 222
