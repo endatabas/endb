1b4e65b96e47f1ad6b461be7d02da6d3986a66a6
Håkan Råberg <hakan.raberg@gmail.com>
2023-12-28 13:36:02 +0100

    Websocket JS client example.

--- examples/endb.mjs
+++ examples/endb.mjs
@@ -73,7 +73,89 @@ class Endb {
     }
 }
 
-export { Endb };
+class EndbWebSocket {
+    constructor(url = 'ws://localhost:3803/sql', {ws, username, password} = {}) {
+        this.ws = ws;
+        if (typeof ws === 'undefined' && typeof WebSocket !== 'undefined') {
+            this.ws = WebSocket;
+        }
+        this.conn = null;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        this.id = 1;
+        this.sentMessages = {};
+        this.pendingMessages = [];
+    }
+
+    async sql(q, p = [], m = false) {
+        if (this.conn === null) {
+            if (this.username && this.password) {
+                this.conn = new this.ws(this.url, encodeURIComponent('Basic ' + btoa(this.username + ":" + this.password)));
+            } else {
+                this.conn = new this.ws(this.url);
+            }
+            this.conn.onerror = (event) => {
+                console.log(event);
+            };
+
+            this.conn.onopen = (event) => {
+                while (this.pendingMessages.length > 0) {
+                    this.conn.send(this.pendingMessages.shift());
+                }
+            };
+
+            this.conn.onclose = (event) => {
+                for (const promise of Object.values(this.sentMessages)) {
+                    promise.reject(new Error('Closed'));
+                }
+            };
+
+            this.conn.onmessage = async (event) => {
+                const message = JSON.parse(await event.data.text(), (k, v) => fromJSONLD(v));
+                if (message['id'] === null) {
+                    console.log(message);
+                } else {
+                    const id = message['id'].toString();
+                    const promise = this.sentMessages[id];
+                    if (promise !== null) {
+                        delete this.sentMessages[id];
+                        if (message.error) {
+                            promise.reject(new Error(message['error']['message']));
+                        } else {
+                            promise.resolve(message['result']);
+                        }
+                    }
+                }
+            };
+        }
+
+        const message = {jsonrpc: '2.0', id: this.id++, method: 'sql', params: {q: q, p: p, m: m}};
+
+        let resolve, reject;
+        const promise = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+        });
+
+        const id = message.id.toString();
+        this.sentMessages[id] = {promise: promise, resolve: resolve, reject: reject};
+
+        const jsonMessage = JSON.stringify(toJSONLD(message));
+
+        if (this.conn.readyState === 1) {
+            this.conn.send(jsonMessage);
+        } else if (this.conn.readyState === 0) {
+            this.pendingMessages.push(jsonMessage);
+        } else {
+            reject(new Error('Closed'));
+        }
+
+        return promise;
+    }
+}
+
+export { Endb, EndbWebSocket };
 
 if (typeof process !== 'undefined' && import.meta.url === `file://${process.argv[1]}`) {
     console.log(await new Endb().sql(process.argv[2]));
