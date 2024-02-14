d0058811aef78e367a025354e7b0c08099926f93
Håkan Råberg <hakan.raberg@gmail.com>
2024-02-14 14:44:27 +0100

    Ensure string to eval from JS is decoded as utf-8 in the wasm console.

--- wasm/endb.c
+++ wasm/endb.c
@@ -6,10 +6,12 @@ static char *c_str_eval_buffer = NULL;
 static cl_fixnum c_str_eval_buffer_size = 0;
 
 char *common_lisp_eval(char *src) {
-  cl_object form = ecl_read_from_cstring(src);
+  cl_object utf_8 = ecl_make_keyword("UTF-8");
+  cl_object lisp_src = ecl_decode_from_cstring(src, -1, utf_8);
+  cl_object form = cl_read_from_string(1, lisp_src);
   cl_object result = si_safe_eval(2, form, ECL_NIL);
   cl_object str =  cl_prin1_to_string(result);
-  cl_fixnum res = ecl_encode_to_cstring(c_str_eval_buffer, c_str_eval_buffer_size, str, ecl_make_keyword("UTF-8"));
+  cl_fixnum res = ecl_encode_to_cstring(c_str_eval_buffer, c_str_eval_buffer_size, str, utf_8);
   if (res > c_str_eval_buffer_size) {
     free(c_str_eval_buffer);
     c_str_eval_buffer = malloc(res);
@@ -17,7 +19,7 @@ char *common_lisp_eval(char *src) {
       abort();
     }
     c_str_eval_buffer_size = res;
-    res = ecl_encode_to_cstring(c_str_eval_buffer, c_str_eval_buffer_size, str, ecl_make_keyword("UTF-8"));
+    res = ecl_encode_to_cstring(c_str_eval_buffer, c_str_eval_buffer_size, str, utf_8);
   }
   if (res == -1) {
     return NULL;
