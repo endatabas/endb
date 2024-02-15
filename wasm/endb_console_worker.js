4fda2204ad33f38fba3f99a2ff139010bbf32f04
Håkan Råberg <hakan.raberg@gmail.com>
2024-02-15 11:01:21 +0100

    No window inside worker.

--- wasm/endb_console_worker.js
+++ wasm/endb_console_worker.js
@@ -1,14 +1,14 @@
-/* global Module */
+/* global Module postCustomMessage */
 
 Module.onCustomMessage = (e) => {
     const { id, method, params } = e.data.userData;
     if (method === "common_lisp_eval") {
         const result = Module.ccall("common_lisp_eval", "string", ["string"], params);
         if (id) {
-            window.postCustomMessage({ id, result });
+            postCustomMessage({ id, result });
         }
     }
 };
 Module.postRun = [() => {
-    window.postCustomMessage({id: "postRun"});
+    postCustomMessage({id: "postRun"});
 }];
